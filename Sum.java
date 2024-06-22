9f08bc2c9e6328fdd8e58777aaee89b99a7564cf
Sriramani14 <satyasriramani@gmail.com>
2024-06-22 15:31:28 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sum{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n,a=0;
+n=s.nextInt();
+for(int i=0;i<=n;i++){
+a=a+i;
+}
+System.out.println(a);
+}
+}
