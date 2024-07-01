df14284b2ee268ef05ca33343f37402b93d68ca3
Sriramani14 <satyasriramani@gmail.com>
2024-07-01 14:10:34 +0530

    Add files via upload

---
+++ StringCharAt2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class StringCharAt2{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        for(int i=s1.length()-1;i>=0;i++){
+        System.out.println(s1.charAt(i));
+        }
+    }
+}
