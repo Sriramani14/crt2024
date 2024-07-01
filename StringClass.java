9ff60d3dd58309abc3cfcd59c5f6e55a7eb17264
Sriramani14 <satyasriramani@gmail.com>
2024-07-01 13:47:34 +0530

    Add files via upload

---
+++ StringClass.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StringCLass{
+    public static void main(String[] args) {
+        String s="java";
+        s=s.concat("lanuage");
+        System.out.println(s);
+       
+    }
+}
