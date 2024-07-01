2ab83f7071737d0b02de4f947c5dbc3d5c5ccff6
Sriramani14 <satyasriramani@gmail.com>
2024-07-01 14:49:55 +0530

    Add files via upload

---
+++ StringEqualsIgnoreCase.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StringEqualsIgnoreCase{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        String s2=s.next();
+        if(s1.equalsIgnoreCase(s2)){
+        System.out.println("Same");
+        }
+        else{
+        System.out.println("not same");
+    }
+    }
+}
