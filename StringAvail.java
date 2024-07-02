9cebee3a17816e9da0addf5440d054534e00fb60
Sriramani14 <satyasriramani@gmail.com>
2024-07-02 15:38:55 +0530

    Add files via upload

---
+++ StringAvail.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class StringAvail{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+       String s2=s.nextLine();
+       if(s1.contains(s2)){
+           System.out.println("String found");
+       }
+       else{
+           System.out.println("String not found");
+       }
+        
+}
+}
