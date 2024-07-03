bbb9cb7ab0b5682b55747d207f82ba76f83223ff
Sriramani14 <satyasriramani@gmail.com>
2024-07-03 13:44:22 +0530

    Add files via upload

---
+++ StringInt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class StringInt{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+       
+    System.out.println(20+40);
+    System.out.println("wel"+"come");
+    System.out.println(20+"python");
+}
+}
