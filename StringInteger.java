651a4cf90ad545a0d941c5c4c61edc3e09340000
Sriramani14 <satyasriramani@gmail.com>
2024-07-03 13:35:38 +0530

    Add files via upload

---
+++ StringInteger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StringInteger{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str="234";
+        String str2="563";
+    System.out.println(Integer.parseInt(str)+Integer.parseInt(str2));
+}
+}
