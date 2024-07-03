836e0d44f5e6b412d50e104aefe1261c558f9d7d
Sriramani14 <satyasriramani@gmail.com>
2024-07-03 13:28:36 +0530

    Add files via upload

---
+++ StringTrim.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StringTrim{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str="          Java          Lang         ";
+           System.out.println(str.length());
+            System.out.println(str);
+            String str1=str.trim();
+             System.out.println(str1.length());
+ System.out.println(str1);
+        
+    
+}
+}
