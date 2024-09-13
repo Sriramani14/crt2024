1423fe7ff6acf655f1891f74382243c629d44064
Sriramani14 <satyasriramani@gmail.com>
2024-09-13 10:30:13 +0530

    Add files via upload

---
+++ DoubleLinklist.java
@@ -0,0 +1,84 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next,prev;
+    Node(int x){
+        data=x;
+        next=null;
+        prev=null;
+    }
+}
+class DoubleList{
+    Node head,tail;
+    DoubleList(){
+        head=tail=null;
+    }
+    void create(int x){
+        Node n=new Node(x);
+        if(head==null){
+            head=tail=n;
+        }
+        else{
+            tail.next=n;
+            n.prev=tail;
+            tail=n;
+        }
+    }
+    void forward(){
+        if(head==null){
+            System.out.println("List is Empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    void backward(){
+        if(head==null){
+            System.out.print("List is empty");
+            return;
+        }
+        Node temp=tail;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.prev;
+        }
+    }
+}
+class DoubleLinklist{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        DoubleList d1=new DoubleList();
+        for(;;){
+            System.out.println();
+            System.out.print("1.Create\n2.Print\n3.Exit\n");
+            int ch=s.nextInt();
+            if(ch==1){
+                System.out.println("Enter element:");
+                int x=s.nextInt();
+                d1.create(x);
+            }
+            else if(ch==2){
+                System.out.println("\n1.Forward\n2.Backward\n");
+                int r=s.nextInt();
+                if(r==1){
+                    d1.forward();
+                }
+                 else if(r==2){
+                    d1.backward();
+                }
+                else{
+                    System.out.print("Invalid Choice");
+                }
+            }
+            else if(ch==3){
+                break;
+            }
+            else{
+                System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
